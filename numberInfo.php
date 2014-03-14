9e3a1382da50865ee9e7930fb2371aba2240a764
MoonAndFox <rachael@moonandfox.com>
2014-03-14 11:30:36 -0700

    add comments

--- numberInfo.php
+++ numberInfo.php
@@ -12,9 +12,10 @@
 	{
 		//check that the user has entered a number of at least 1 character
 		$firstLength = strlen($_GET['num']);
-		//if name ok, carry out tasks
+		//if number ok, carry out tasks
 		if($firstLength > 0)
-		{
+		{	
+			//test for odd or even
 			$numInput = $_GET['num'];
 			$remainder = $numInput % 2 ;
 			if ($remainder == 0)
@@ -27,17 +28,20 @@
 				$attributes['odd']  = true ;
 				$attributes['even']  = false ;
 			}
-				
+			
+			//test for palindrome
 			if(strrev($numInput)==$numInput)
 				$attributes['palindrome']  = true ;
 			else
 				$attributes['palindrome']  = false ;
-				
+			
+			//call isPrime function to test for prime
 			if(isPrime($numInput))
 				$attributes['prime']  = true;
 			else
 				$attributes['prime']  = false;
-				
+			
+			//test for palindrome prime. If true, set individual features to false
 			if(($attributes['palindrome']) &&  ($attributes['prime']))
 			{
 				$attributes['PalindromePrime']  = true;
@@ -47,7 +51,7 @@
 			else
 				$attributes['PalindromePrime']  = false;
 			
-			
+			//populate an array and keep count of true attributes of number
 			$i=1;
 			$atts=0;
 			foreach($attributes as $key => $value)
@@ -59,17 +63,22 @@
 				}
 			}
 			echo "The number ".$numInput." is ";
+			
+			//print out the key if the feature is true
 			foreach($attributes as $key => $value) 
 			{
 				if($value)
 				{
 					echo $key;
+					//print the word 'and' after the feature if there is more than one, and current is not the last
 					if ((count($attributes)>1) && $i<($atts))
 						echo " and ";	
 					$i++;
 				}
 			}
 		}
+		
+		//write an error message if no number entered
 		else
 		{
 			echo'<span style = "color:red">*Please Enter a Number.</span>';
